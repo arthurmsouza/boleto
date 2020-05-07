1e34d3c1c3a3918eb1f2c8480abd142e7d0f60c4
Arthur <arthur.msouza@gmail.com>
2020-05-07 13:54:31 -0300

    ajustes enrollment2

--- boleto-backoffice/app.js
+++ boleto-backoffice/app.js
@@ -85,7 +85,6 @@ const helloAsync = async () => {
 // Setting for Hyperledger Fabric
 const { FileSystemWallet, Gateway } = require('fabric-network');
 const fs = require('fs');
-const path = require('path');
 const ccpPath = path.resolve(__dirname, '..', '..', 'basic-network', 'connection.json');
 const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
 const ccp = JSON.parse(ccpJSON);
