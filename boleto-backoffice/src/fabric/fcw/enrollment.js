1be7a092532ba501445e135f2fb50dbb1b89e7f2
Arthur <arthur.msouza@gmail.com>
2020-05-07 12:16:56 -0300

    ajustes enrollment

--- boleto-backoffice/src/fabric/fcw/enrollment.js
+++ boleto-backoffice/src/fabric/fcw/enrollment.js
@@ -44,10 +44,36 @@ module.exports = function(logger) {
     var chainCodeEnroll =  async function (options,cb){
     try { 
         
-        var resp = null;
+        
         console.log('####network asyn teste');
-        var networkPromise = await connection();
-        console.log('####network asyn teste2', networkPromise);
+        //var networkPromise = await connection();
+        
+         // Create a new file system based wallet for managing identities.
+         const walletPath = path.join(process.cwd(), 'wallet');
+         const wallet = new FileSystemWallet(walletPath);
+         console.log(`xxxWallet path: ${walletPath}`);
+        
+         // Check to see if we've already enrolled the user.
+         console.log('####user');
+         const userExists = await wallet.exists('user1');
+         console.log('####use2',userExists);
+         if (!userExists) {
+             console.log('An identity for the user "user1" do not exists in the wallet');
+             return;
+         }
+         console.log('####user',userExists);
+         console.log('####gateway');
+         // Create a new gateway for connecting to our peer node.
+         const gateway = new Gateway();
+         await gateway.connect(ccp, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: false } });
+         console.log('####gateway fim');
+         // Get the network (channel) our contract is deployed to.
+         const network = await gateway.getNetwork('mychannel');
+         console.log('####network async teste2', network);
+          // Get the contract from the network.
+         //const contract = network.getContract('boleto');
+        
+       // console.log('####network asyn teste2', networkPromise);
        // networkPromise.then(network => {
        //     console.log('####print network',network);
        //     resp = network;
